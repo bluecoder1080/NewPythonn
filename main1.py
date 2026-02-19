afdc4ef20d70531cff8f84f624729f20e64556bf
adityasingh1080z@gmail.com <adityasingh1080z@gmail.com>
2026-02-20 05:10:13 +0530

    new

---
+++ main1.py
@@ -0,0 +1,17 @@
+def find_max(numbers):
+    max = numbers[0]
+
+    for i in range(1, len(numbers)):
+        if numbers[i] > max_value:
+            max = numbers[i]
+
+    return max
+
+
+nums = [5, 8, 2, 15, 3]
+
+result = find_max(nums)
+
+print("Maximum number is:", result)
+
+print("Length is
